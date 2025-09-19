00ec7a87365e29f8ed984bdcabef56b691c82623
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-19 18:28:43 +0200

    Update cords_generator.py

--- cords_generator.py
+++ cords_generator.py
@@ -1,8 +1,43 @@
 tones = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']
 
-
+print()
 for index, tone in enumerate(tones):
     if index == len(tones) - 1:
-        print(tone)
+        print(index + 1, tone)
     else:
-        print(tone, end=', ')
+        print(index + 1, tone, end=', ')
+
+print()
+users_tone = input('Upisite ton iz liste za koji zelite generirati akord: ')
+
+# dur 0, 4, 7
+# mol 0, 3, 7
+
+index_users_tone = tones.index(users_tone.upper())
+
+# Nacin uporabom % operatora
+# first_tone = tones[index_users_tone]
+# dur_middle_tome = tones[((index_users_tone + 4) % 12)]
+# mol_middle_tome = tones[((index_users_tone + 3) % 12)]
+# last_tone = tones[((index_users_tone + 7) % 12)]
+
+# Nacin uporabom .extend() metode
+# tones.extend(tones)
+# first_tone = tones[index_users_tone]
+# dur_middle_tome = tones[index_users_tone + 4]
+# mol_middle_tome = tones[index_users_tone + 3]
+# last_tone = tones[index_users_tone + 7]
+
+# Nacin oduzimanjem duzine liste od indeksa selektiranog tona
+first_tone = tones[index_users_tone]
+dur_middle_tome = tones[(index_users_tone + 4) - len(tones)]
+mol_middle_tome = tones[(index_users_tone + 3) - len(tones)]
+last_tone = tones[(index_users_tone + 7) - len(tones)]
+
+dur_message = f'{users_tone.upper()} ({first_tone}, {dur_middle_tome}, {last_tone})'
+mol_message = f'{users_tone.upper()} ({first_tone}, {mol_middle_tome}, {last_tone})'
+
+print()
+print(dur_message)
+print(mol_message)
+print()
